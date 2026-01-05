433e7ed1389096debdb091b50254bd18ceef3335
pehgm <pedrohgiudice@gmail.com>
2026-01-05 00:00:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ const closeBtn = document.querySelector('.close-btn');
 // Abrir Janelinha
 toggleBtn.addEventListener('click', () => {
     modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
 });
 
 //Fechar Janelinha
 closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
 });
 
 // Fechar clicando fora da janelinha
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.style.display = 'none';
+        document.body.style.overflow = 'auto';
+
     }
 });
